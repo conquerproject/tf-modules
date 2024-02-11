02b450934326a109a02d112ee5c3a47470ce8eba
benhur-araujo <benhur.araujo.silva@gmail.com>
2024-02-11 11:37:51 -0300

    feat: New module for gh repos creation
    Issue #6

---
+++ github-repositories/main.tf
@@ -0,0 +1,15 @@
+provider "github" {
+  owner = var.gh_organization
+}
+
+resource "github_repository" "repos" {
+  for_each           = var.repos
+  name               = each.key
+  description        = each.value.description
+  visibility         = each.value.visibility
+  allow_merge_commit = each.value.allow_merge_commit
+  allow_squash_merge = each.value.allow_squash_merge
+  allow_rebase_merge = each.value.allow_rebase_merge
+  has_wiki           = try(each.value.has_wiki, false)
+}
+
---
+++ github-repositories/variables.tf
@@ -0,0 +1,16 @@
+variable "gh_organization" {
+  type = string
+}
+
+variable "repos" {
+  description = "GitHub Repositories"
+  type = map(object({
+    description        = string
+    visibility         = string
+    allow_merge_commit = bool
+    allow_squash_merge = bool
+    allow_rebase_merge = bool
+    has_wiki = optional(bool)
+  }))
+}
+
