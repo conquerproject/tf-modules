250f29c8e72731e0124b4c75cded28ba9c54197e
benhur-araujo <benhur.araujo.silva@gmail.com>
2024-04-01 08:49:51 -0300

    feat(gh-repos): add branch protection
    GH Issue #6

--- github-repositories/main.tf
+++ github-repositories/main.tf
@@ -14,3 +14,14 @@ resource "github_repository" "repos" {
   has_issues         = each.value.has_issues
 }
 
+resource "github_branch_protection" "main" {
+  for_each = var.repos
+  repository_id = each.key
+  pattern = "main"
+  enforce_admins = try(each.value.branch_protection.enforce_admins, true)
+  required_linear_history = try(each.value.branch_protection.linear_history, true)
+  require_conversation_resolution =try(each.value.branch_protection.conversation_resolution, true)
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+}
